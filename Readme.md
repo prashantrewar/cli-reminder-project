918cc3d428ef612250e77b1d35377e9f0073c1dd
prashantrewar <prashant09032001@gmail.com>
2023-06-23 01:11:14 +0530

    simple cli-reminder alert

---
+++ Readme.md
@@ -0,0 +1,5 @@
+The CLI Reminder project is a command-line interface (CLI) application
+designed to help users manage and organize their tasks, events, and
+deadlines efficiently. It serves as a lightweight and versatile tool for creating,
+viewing, and managing reminders without the need for a graphical user
+interface.
---
+++ cmd/root.go
@@ -0,0 +1,46 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "cli-reminder-project",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	
+	Run: func(cmd *cobra.Command, args []string) {fmt.Println("Hello CLI") },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func initConfig() {
+	
+}
+ 
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
